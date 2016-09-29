7fb8c855c61689594f1c3a02d397be29cd970649
deepjahan <deepjahan.grewal@research.iiit.ac.in>
2016-09-29 20:59:23 +0530

    Adding file for discounted best bid

--- BestBid_discounted.java
+++ BestBid_discounted.java
@@ -6,16 +6,18 @@ import negotiator.AgentID;
 import negotiator.actions.Accept;
 import negotiator.actions.Action;
 import negotiator.actions.Offer;
+import negotiator.actions.EndNegotiation;
 import negotiator.parties.AbstractNegotiationParty;
 import negotiator.utility.AbstractUtilitySpace;
 import negotiator.Deadline;
 import negotiator.session.TimeLineInfo;
 import negotiator.Bid;
+import java.lang.Math;
 
 /**
  * This is your negotiation party.
  */
-public class ExponentialBoulware extends AbstractNegotiationParty {
+public class BestBid_discounted extends AbstractNegotiationParty {
 
 	private double discountFactor = 0; // if you want to keep the discount
 										// factor
@@ -28,11 +30,6 @@ public class ExponentialBoulware extends AbstractNegotiationParty {
 	@Override
 	public void init(AbstractUtilitySpace utilSpace, Deadline dl,
 			TimeLineInfo tl, long randomSeed, AgentID agentId) {
-
-		System.out.println("Deadline is " + dl);
-		System.out.println("Deadline is " + dl.getType());
-		System.out.println("Deadline is " + dl.getValue()*0.85);
-
 
 		super.init(utilSpace, dl, tl, randomSeed, agentId);
 
@@ -48,17 +45,17 @@ public class ExponentialBoulware extends AbstractNegotiationParty {
 
 	}
 
-	private Bid getRandomBid(double target) {
+	private Bid getBestBid() {
 		Bid bid = null;
-		//try {
-			int loops = 0;
-			do {
-				bid = utilitySpace.getDomain().getRandomBid();
-				loops++;
-			} while (loops < 100000 && utilitySpace.getUtility(bid) < target);
-			if (bid == null) { 
-				bid=utilitySpace.getDomain().getRandomBid();
+		try {
+			// bid = utilitySpace.getDomain().getRandomBid();
+			if (maxBid == null) {
+				maxBid = utilitySpace.getMaxUtilityBid();
 			}
+			bid = maxBid;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return bid;
 	}
 
@@ -71,29 +68,41 @@ public class ExponentialBoulware extends AbstractNegotiationParty {
 	 *            Either a list containing both accept and offer or only offer.
 	 * @return The chosen action.
 	 */
-
-	private double expUtility() {
-		double incr_point=0.8;
-		double e=java.lang.Math.E;
-		return (-java.lang.Math.exp(timeline.getTime()-incr_point)*(1-reservationValue)/java.lang.Math.exp(1-incr_point)+1-reservationValue)*e/(e-1)+reservationValue;
-	}
-
 	@Override
 	public Action chooseAction(List<Class<? extends Action>> validActions) {
-		// with 50% chance, counter offer
-		// if we are the first party, also offer.
 
-		//if (opponentBid != null && getUtility(opponentBid) > targetUtility)
-		//	targetUtility = getUtility(opponentBid);
-		targetUtility=expUtility();
-		System.out.println("Target Utility (post op): " + targetUtility);
-
-		if (!validActions.contains(Accept.class) || getUtility(opponentBid) <= 1.0) {
-			Bid selfBid = getRandomBid(targetUtility);
-			return new Offer(selfBid);
+		if(discountFactor==1.0)
+		{
+			if (timeline.getTime() < 1.0) {
+				Bid selfBid = getBestBid();
+				return new Offer(selfBid);
+			} 
+			else {
+				return new Accept();
+			}
 		}
-		else {
-			return new Accept();
+		else
+		{
+			System.out.println("utility: "+getUtility(opponentBid)+", discounted utility: "+getUtilityWithDiscount(opponentBid)+", d^currentTime: "+(Math.pow(discountFactor,timeline.getCurrentTime()-2)));
+			if (getUtilityWithDiscount(opponentBid)>Math.pow(discountFactor,timeline.getCurrentTime()-2)) {
+				//System.out.println("1 utility: "+getUtility(opponentBid)+", discounted utility: "+getUtilityWithDiscount(opponentBid)+", d^currentTime: "+(Math.pow(discountFactor,timeline.getCurrentTime()-2)));
+				//return new Accept();
+				Bid selfBid = getBestBid();
+				return new Offer(selfBid);
+			} 
+			else {
+				Bid selfBid = getBestBid();
+				if(getUtilityWithDiscount(selfBid)>reservationValue)
+				{
+					//System.out.println("2");
+					return new Offer(selfBid);
+				}
+				else
+				{
+					//System.out.println("3");
+					return new EndNegotiation();
+				}
+			}
 		}
 	}
 
